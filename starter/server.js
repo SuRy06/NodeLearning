b7ca5d3e44c310e809ed06eb3fa67704895e0338
Sudeep Chowdhury <codeingsury@gmail.com>
2023-01-01 20:06:53 +0530

    mongo db

--- starter/server.js
+++ starter/server.js
@@ -2,9 +2,8 @@ const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
-console.log(process.env);
 
-const port = 3000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+  // console.log(`App running on port ${port}...`);
 });
