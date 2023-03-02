f37ca973a956cbaed14719da9c94793c41b15ecc
Sudeep Chowdhury <codeingsury@gmail.com>
2023-03-02 17:56:45 +0530

    gogo

--- starter/controllers/userController.js
+++ starter/controllers/userController.js
@@ -1,5 +1,15 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if(allowedFields.includes(el)) newObj[el] = obj[el]
+  });
+
+  return newObj
+}
 
 exports.getAllUsers =catchAsync(async (req, res, next) => {
   const users = await User.find();
@@ -14,6 +24,36 @@ exports.getAllUsers =catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.updateMe = catchAsync(async(req, res, next) => {
+  // 1)CREATE ERROR IF USER POSTS PASSWORD DATA
+  if(req.body.password || req.body.passwordConfirm){
+    return next(new AppError('This route is not for password updates. Please use /updateMyPassword',400))
+  }
+
+  // 2)FILTERED OUT UNWANTED FIELD NAMES THAT ARE NOT ALLOWED TO BE UPDATED
+  const filteredBody = filterObj(req.body, 'name', 'email')
+
+  // 3)UPDATE USER DOCUMENT
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {new: true, runValidators: true});
+
+  res.status(200).json({
+    status:'sucsess',
+    data: {
+      user: updatedUser
+    }
+  })
+})
+
+exports.deleteMe = catchAsync(async(req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id,{'active':false})
+
+  res.status(204).json({
+    status: 'success',
+    data: null
+  })
+})
+
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'err',
--- starter/models/userModel.js
+++ starter/models/userModel.js
@@ -46,6 +46,11 @@ const userSchema = new mongoose.Schema({
   passwordChangedAt: Date,
   passwordResetToken: String,
   passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false
+  }
 });
 
 userSchema.pre('save', async function (next) {
@@ -66,6 +71,12 @@ userSchema.pre('save', function (next) {
   this.passwordChangedAt = Date.now() - 1000;
   next();
 });
+
+userSchema.pre(/^find/, function(next) {
+  // THIS POINTS TO THE CURRENT QUERY
+  this.find({active: {$ne:false}});
+  next();
+})
 
 userSchema.methods.correctPassword = async function (
   candidatePassword,
